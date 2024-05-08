c4eb9104583e0e815552416109787975e3d88c05
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-05-08 22:29:34 +0300

    Header files has been

---
+++ process.h
@@ -0,0 +1,16 @@
+// process.h
+#ifndef PROCESS_H
+#define PROCESS_H
+
+// Define process structure
+typedef struct {
+    char name[10];
+    int arrival_time;
+    int priority;
+    int burst_time;
+    int ram_required;
+    int cpu_percentage;
+    int remaining_time;
+} Process;
+
+#endif //PROCESS_H
